920b5933e6ae0db5e7edc98d4437f1a700f02d79
Aman <zippfake@gmail.com>
2026-09-08 11:49:02 -0700

    Add PDF text extraction

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import pymupdf
 
+def extract_text_from_pdf(uploaded_file):
+    pdfbytes = uploaded_file.read()
+    document = pymupdf.open(
+        stream = pdfbytes,
+        filetype= "pdf"
+    )
+    text = ""
+    for page in document:
+        text += page.get_text()
+
+    document.close()
+
+    return text
+
 st.set_page_config(
     page_title="EvalRAG AI",
     page_icon="🤖",
@@ -25,3 +39,8 @@ if uploaded_file is not None:
     st.write("File name", uploaded_file.name)
     st.write("File type", uploaded_file.type)
     st.write("File size", uploaded_file.size, "bytes")
+
+    extracted_text = extract_text_from_pdf(uploaded_file)
+
+    st.subheader = "Extracted Text Preview"
+    st.text(extracted_text[:2000])
